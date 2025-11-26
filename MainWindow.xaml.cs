a0c2789e6cf2cd5cd91eaf599535443ca431a378
srobertogo-dot <srobertogo@gmail.com>
2025-11-26 13:42:51 -0400

    este es el proyecto

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDataBlindingP2C1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       public Producto producto { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Producto prod1 = new Producto("refrigerador", 3450.8);
+        }
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppDataBlindingP2C1
+{
+    public class Producto 
+    {
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+
+        public Producto()
+        {
+            Nombre = "no definido";
+            Precio = 0;
+
+        }
+        public Producto(string nom, double pre)
+        {
+            Nombre = nom;
+            Precio = pre;
+        }
+        public string verDatos()
+        {
+            return (Nombre + " " + Precio.ToString());
+        }
+    }
+}
